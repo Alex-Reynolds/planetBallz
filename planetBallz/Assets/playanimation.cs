362bde137eb180a3d02e5b0faba7f161a68b6b59
jeffhhhh <jeffreyshawnremy@gmail.com>
2021-11-14 13:17:49 -0600

    Added start animation screen and fixed restart button

---
+++ planetBallz/Assets/playanimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class playanimation : MonoBehaviour
+{
+    VideoPlayer video;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
