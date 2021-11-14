17408fe4587bb288db42e59cd3b121c6ff5b278e
jeffhhhh <jeffreyshawnremy@gmail.com>
2021-11-14 02:14:34 -0600

    Cowboy merge, added a couple of game objects is all

--- planetBallz/Assets/timer.cs
+++ planetBallz/Assets/timer.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
 
 public class timer : MonoBehaviour
 {
+
+  
+    int endTime;   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DateTime dt = DateTime.Now;
+        endTime = (dt.Second +  59)%60;
     }
 
     // Update is called once per frame
     void Update()
     {
+        DateTime dt = DateTime.Now;
+        if (dt.Second == endTime) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+
+        if (dt.Second > endTime){
+           Text t = gameObject.GetComponent<Text>(); 
+           t.text = "OBJECTIVE SURVIVE for " + (dt.Second - endTime) + " more seconds";
+        } else {
+            Text t = gameObject.GetComponent<Text>();
+            t.text = "OBJECTIVE SURVIVE for " + ((endTime + 59) - dt.Second) + " more seconds";
+        }
         
+
     }
 }
